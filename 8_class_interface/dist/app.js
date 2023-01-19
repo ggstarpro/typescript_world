4c31470125a5f9cf91440b44248295e710dc1fa2
GentaMorino <genta.morino@gmail.com>
2023-01-20 02:39:54 +0900

    interface

--- 8_class_interface/dist/app.js
+++ 8_class_interface/dist/app.js
@@ -1,65 +1,24 @@
 "use strict";
-class BaseDepartment3 {
-    constructor(id, name) {
-        this.id = id;
+class PersonTest {
+    constructor(name, age = 20) {
         this.name = name;
-        this.employees = [];
+        this.age = age;
     }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInfoMation() {
-        console.log(`長さ:${this.employees.length} 中身:${this.employees}`);
-    }
-    static createEmployee(name) {
-        return { name: name };
+    greet(phrase) {
+        console.log(phrase + '私の名前は' + this.name + this.age + '歳です');
     }
 }
-BaseDepartment3.fisicalYear = 2020;
-class AccountingDepartment2 extends BaseDepartment3 {
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('レポートが見つかりません。');
+let user1;
+user1 = {
+    name: '名前',
+    greet(phrase) {
+        console.log(phrase + '>>' + this.name);
     }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error('正しい値を設定してください。');
-        }
-        this.addReport(value);
-    }
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment2(Math.random().toString(), []);
-        return this.instance;
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-    addEmployee(name) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name);
-    }
-    describe() {
-        console.log('AccountingDepartment>>' + this.id);
-    }
-}
-const accountingDepartment2 = AccountingDepartment2.getInstance();
-const accountingDepartmentTest = AccountingDepartment2.getInstance();
-console.log(accountingDepartment2);
-console.log(accountingDepartmentTest);
+};
+user1.greet('Hello>>');
+let user2;
+user2 = new PersonTest('天寸都');
+user2.greet('Hello>>');
+console.log(user1);
+console.log(user2);
 //# sourceMappingURL=app.js.map
--- 8_class_interface/src/app.ts
+++ 8_class_interface/src/app.ts
@@ -1,89 +1,43 @@
-// 抽象クラスはインスタンス化できない。継承したものがインスタンス化できる
-abstract class BaseDepartment3 {
-  static fisicalYear = 2020;
+// Q.typeを使っても同じことができるがなぜ必要か？？？
+// interfaceはオブジェクトの構造を記述するためだけに使える。
+// Q.abstructとの違いは?
+// interfaceは何も実装を持たない, absctuctは抽象も具体的な実装も持てる。
+interface PersonInterface {
+  // 初期化はできない
+  // name: string = 'person';
+  name: string;
+  greet(phrase: string): void;
+}
 
-  protected employees: string[] = [];
-  constructor(
-    protected readonly id: string,
-    protected name: string,
-  ) {
+type PersonType = {
+  name: string;
+  greet(phrase: string): void;
+}
+
+class PersonTest implements PersonInterface {
+  constructor(public name: string, public age: number = 20) {
+
   }
-
-  abstract describe(this: BaseDepartment3): void;
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInfoMation() {
-    console.log(`長さ:${this.employees.length} 中身:${this.employees}`);
-  }
-
-  // static(newしなくても使える)
-  static createEmployee(name: string) {
-    return {name: name};
+  greet(phrase: string): void {
+    console.log(phrase + '私の名前は' + this.name + this.age + '歳です');
   }
 }
 
-// 会社の中に１つしかないのでオブジェクトは１つしか作りたくないため、newで1回だけしか作れないようにしたい。=> private constructor
-class AccountingDepartment2 extends BaseDepartment3 {
-  private lastReport: string;
-  private static instance: AccountingDepartment2;
-
-  // ゲッター:何かを返さなければいけない
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error('レポートが見つかりません。');
-  }
-
-  // セッター
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error('正しい値を設定してください。');
-    }
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    // AccountingDepartment2.instanceでも良い
-    if (this.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment2(Math.random().toString(), []);
-    return this.instance;
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-
-  addEmployee(name: string) {
-    if (name === 'Max') {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  describe() {
-    console.log('AccountingDepartment>>' + this.id);
+//
+let user1: PersonInterface;
+user1 = {
+  name: '名前',
+  greet(phrase: string) {
+    console.log(phrase + '>>' + this.name);
   }
 }
+user1.greet('Hello>>');
 
-// クラス 'AccountingDepartment2' のコンストラクターはプライベートであり、クラス宣言内でのみアクセス可能です。ts(2673)
-// const accountingDepartment2 = new AccountingDepartment2('d2', []);
-const accountingDepartment2 = AccountingDepartment2.getInstance();
-const accountingDepartmentTest = AccountingDepartment2.getInstance();
-console.log(accountingDepartment2);
-console.log(accountingDepartmentTest);
+//
+let user2: PersonInterface;
+user2 = new PersonTest('天寸都');
+user2.greet('Hello>>');
+
+//
+console.log(user1); // {name: '名前', greet: ƒ}
+console.log(user2); // PersonTest {name: '天寸都', age: 20}
